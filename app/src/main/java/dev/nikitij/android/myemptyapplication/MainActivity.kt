a1f5282fb922ed4ded972e2f3cb64d244c98e352
DESKTOP-KKUBLPT\Nikita <nikitij.dev>
2020-11-08 17:24:36 +0400

    fragments added

--- app/src/main/java/dev/nikitij/android/myemptyapplication/MainActivity.kt
+++ app/src/main/java/dev/nikitij/android/myemptyapplication/MainActivity.kt
@@ -2,10 +2,15 @@ package dev.nikitij.android.myemptyapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.nikitij.android.myemptyapplication.fragments.FragmentA
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentsContainer, FragmentA())
+            .commit()
     }
 }
